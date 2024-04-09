0810748c299ee4f7d5b9e794f834215c85688a98
patrik-bolander-dc <patrik.bolander@datacom.co.nz>
2024-04-09 16:00:04 +1200

    tidy up ui

--- app/tool2/page.tsx
+++ app/tool2/page.tsx
@@ -2,37 +2,14 @@ import FileUploader from '@/components/file-upload/file-upload-component';
 import SingleFileUploader from '@/components/file-upload/single-file-upload';
 import React from 'react'
 
-// const url = "/api/upload";
-const url = 'https://rekognition-backend.azurewebsites.net/analyze'
 
-interface ContainerProps {
-  children: React.ReactNode
-}
-
-const Container = ({ children }: ContainerProps) => (
-  <div className="flex flex-col items-center justify-between gap-4 min-h-52 bg-zinc-800 w-full max-w-2xl py-10 px-10 rounded-xl h-fit">
-    {children}
-  </div>
-)
 
 function Tool2() {
   return (
     <div className='w-full flex justify-center'>
-
-      {/* <Container>
-        <h1 className="text-2xl font-bold">File Uploader</h1>
-        <FileUploader
-          url={url}
-          acceptedFileTypes={[
-            "image/png",
-            "image/jpeg",
-          ]}
-          maxFileSize={100} // in MB
-          label="Max File Size: 1GB" 
-          labelAlt="Accepted File Types: png, jpeg"
-        />
-      </Container> */}
-      <SingleFileUploader />
+      <SingleFileUploader 
+      label="Max File Size: 1GB" 
+      labelAlt="Accepted File Types: png, jpeg"/>
     </div>
   )
 }
--- components/file-upload/single-file-upload.tsx
+++ components/file-upload/single-file-upload.tsx
@@ -1,9 +1,17 @@
 "use client"
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import Image from 'next/image'
 
-const SingleFileUploader = () => {
+interface SingleFileUploaderProps {
+  label: string
+  labelAlt: string
+}
+
+
+const SingleFileUploader = ({ label, labelAlt }: SingleFileUploaderProps) => {
   const [file, setFile] = useState<File | null>(null);
-  const [img, setImg] = useState('');
+  const [imgResult, setImgResult] = useState('');
+  const [uploadError, setUploadError] = useState<string | null | unknown>(null);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
@@ -11,77 +19,121 @@ const SingleFileUploader = () => {
     }
   };
 
-  useEffect(() => {
-    file
-    console.log(file)
-  }, [file])
-  
-
-
   const handleUpload = async () => {
     if (file) {
       console.log("Uploading file...");
-      console.log(file)
-  
+
       const formData = new FormData();
       formData.append("image", file);
-  
+
       const url = 'https://rekognition-backend.azurewebsites.net/analyze'
-  
-      for (const value of formData.entries()) {
-        console.log('item:', value);
-      }
-  
+
       try {
         const result = await fetch(url, {
           method: "POST",
           body: formData
         });
-  
+
+        if (!result.ok) {
+          console.error('Upload failed:', await result.text());
+          setUploadError(await result.text());
+          return
+        }
+
         if (result.ok) {
           const imageBlob = await result.blob();
 
           const imageObjectURL = URL.createObjectURL(imageBlob);
-          setImg(imageObjectURL);
-        } else {
-          console.error('Upload failed:', await result.text());
+          setImgResult(imageObjectURL);
         }
+
       } catch (error) {
-        console.log('here in error')
         console.error(error);
+        setUploadError(error);
       }
     }
   };
-  
+
+  const resetImageUpload = () => {
+    setImgResult('')
+    setFile(null)
+    setUploadError(null)
+  }
+
+  function formatBytes(bytes: number) {
+    return (bytes / Math.pow(1024, 2)).toFixed(3)
+  }
+
   return (
-    <>
-      <div>
-        <label htmlFor="file" className="sr-only">
-          Choose a file
+    <div className="w-3/4 md:w-full  flex flex-col items-center ">
+
+      <div className=" w-full md:w-1/2 flex flex-col bg-gray-900 p-3 rounded-xl max-w-2xl min-w-20">
+        <label className="flex justify-between text-blue-200">
+          <span>{label}</span>
+          <span>{labelAlt}</span>
         </label>
-        <input id="file" type="file" onChange={handleFileChange} />
+        <input type="file"
+          className='bg-dcBlue rounded-xl border border-blue-500 text-lg
+                            file:bg-blue-500 file:border-0 file:p-3 file:px-4 file:rounded-l file:text-black file:font-semibold file:text-lg file:mr-5
+                            file:hover:cursor-pointer'
+          onChange={handleFileChange}
+        />
+
+        <label className="label">
+          <span className="label-text-alt text-red-500">{uploadError as string}</span>
+        </label>
+
+        {file && (
+          <section className="w-full py-5 flex justify-between animate-fade-down animate-once animate-duration-1000 animate-delay-100 ">
+            <div className="">
+              <h3 className="font-semibold">File details</h3>
+              <ul>
+                <li><span className="font-semibold pr-1">Name:</span> {file.name}</li>
+                <li><span className="font-semibold pr-1">Type:</span> {file.type}</li>
+                <li><span className="font-semibold pr-1">Size:</span> {formatBytes(file.size)} MG</li>
+              </ul>
+            </div>
+            <div className="rounded-xl">
+              <Image
+                src={URL.createObjectURL(file)}
+                width={150}
+                height={150}
+                alt="Picture of the author"
+                className="rounded-xl"
+              />
+            </div>
+          </section>
+        )}
+
+        
+        {file && (
+          <button className='bg-gray-200 text-black font-semibold rounded-lg py-1' onClick={imgResult ? resetImageUpload : handleUpload}>
+            {imgResult ? <p>Upload another file</p> : <p>Upload file</p>}
+          </button>
+        )}
+
+        {uploadError !== null && (
+          <div className="w-full text-red-500 font-semibold ">
+            <p className="">An error has occurred</p>
+            <p className="">{uploadError as string}</p>
+          </div>
+        )}
+
       </div>
-      {file && (
-        <section>
-          File details:
-          <ul>
-            <li>Name: {file.name}</li>
-            <li>Type: {file.type}</li>
-            <li>Size: {file.size} bytes</li>
-          </ul>
-        </section>
-      )}
 
-      {file && <button className='bg-gray-900 rounded-lg' onClick={handleUpload}>Upload a file</button>}
+      <section className="w-full mt-5 max-w-2xl">
+        {imgResult !== '' && (
+          <div>
+            <h1>Result:</h1>
+            <img src={imgResult} alt="result image" />
+          </div>
+        )}
+      </section>
 
-      {img !== '' && (
-        <img src={img} alt="result image" />
-      )
-
-      }
-
-    </>
+    </div>
   );
 };
 
 export default SingleFileUploader;
+
+
