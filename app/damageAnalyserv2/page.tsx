66994778f1ecaef24940c9441519b4deb112bb91
patrik-bolander-dc <patrik.bolander@datacom.co.nz>
2024-05-02 16:47:59 +1200

    code tidyup

--- app/damageAnalyserv2/page.tsx
+++ app/damageAnalyserv2/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import SingleFileUploader from '@/components/multiple-file-upload/single-file-upload';
 import React, { useEffect, useState } from 'react'
-import { carPartType, carSide } from '@/types';
+import {carSide } from '@/types';
 import ListOfCarParts from '@/components/multiple-file-upload/list-of-parts';
 import {
   BackFile,
@@ -14,7 +14,6 @@ import {
   LeftFile,
   LeftImageResult,
   RegoNumber,
-  ResetAllAtoms,
   RightFile,
   RightImageResult,
   UploadError
@@ -34,8 +33,6 @@ function MulitpleDamageAnalyser() {
   const multipleImageUpload = 'https://rekognition-backend.azurewebsites.net/damage_analysis';
   const analyzeImageUrl = 'https://rekognition-backend.azurewebsites.net/analyze';
 
-  const [carPartArray, setCarPartArray] = useState<carPartType[]>([]);
-
   const [frontFile, setFrontFile] = useAtom(FrontFile);
   const [leftFile, setLeftFile] = useAtom(LeftFile);
   const [rightFile, setRightFile] = useAtom(RightFile);
@@ -47,16 +44,15 @@ function MulitpleDamageAnalyser() {
   const [backImageResult, setBackImageResult] = useAtom(BackImageResult);
   const [jsonResult, setJsonResult] = useAtom(JsonResult)
 
-  const [regoNumber, setRegoNumber] = useAtom(RegoNumber)
+  const [regoNumber, setRegoNumber] = useAtom(RegoNumber);
 
   const [uploadError, setUploadError] = useAtom(UploadError);
   const [isUploading, setIsUploading] = useAtom(IsUploading);
 
-  const [isCameraActive, setIsCameraActive] = useAtom(IsCameraActive);
+  const [isCameraActive, setIsCameraActive] = useAtom(IsCameraActive); // TODO
 
   const isAnyFilesSelected = frontFile || leftFile || rightFile || backFile;
   const isAllFilesSelected = frontFile && leftFile && rightFile && backFile;
-  const hasAnyImageResult = frontImageResult || leftImageResult || rightImageResult || backImageResult;
 
   const [effectedCarParts, setEffectedCarParts] = useState<carSide[]>()
 
@@ -69,7 +65,7 @@ function MulitpleDamageAnalyser() {
 
   const handleAllFileUploads = async () => {
     if (!isAnyFilesSelected) {
-      setUploadError('Please select an image to upload');
+      setUploadError('Please select 4 images to upload');
       return
     }
 
@@ -101,21 +97,8 @@ function MulitpleDamageAnalyser() {
       }
 
       if (result.ok) {
-        // const imageBlob = await result.blob();
         const response = await result.json();
         setJsonResult(response);
-        // const imageObjectURL = URL.createObjectURL(imageBlob);
-        // const frontImageObjectURL = URL.createObjectURL(imageBlob);
-        // const leftImageObjectURL = URL.createObjectURL(imageBlob);
-        // const rightImageObjectURL = URL.createObjectURL(imageBlob);
-        // const backImageObjectURL = URL.createObjectURL(imageBlob);
-
-        // setImgResult(imageObjectURL);
-        // setFrontImageResult(frontImageObjectURL);
-        // setLeftImageResult(leftImageObjectURL);
-        // setRightImageResult(rightImageObjectURL);
-        // setBackImageResult(backImageObjectURL);
-
         setIsUploading(false);
       }
 
@@ -171,9 +154,9 @@ function MulitpleDamageAnalyser() {
     setRegoNumber(e.target.value);
   }
 
-  // useEffect(() => {
-  //   setJsonResult(null)
-  // }, [])
+  useEffect(() => {
+    resetAllAtoms()
+  }, [])
 
   const analyseVehicleImage = async (file: File, side: carSide) => {
 
@@ -249,8 +232,8 @@ function MulitpleDamageAnalyser() {
         {/* Upload Button */}
         {isAllFilesSelected && (
           <button className='bg-green-500 text-white dark:bg-green-300 dark:text-gray-900 font-semibold rounded-lg p-3' onClick={jsonResult ? resetAllAtoms : handleAllFileUploads}>
-            {!jsonResult && !isUploading && (<p>Upload file</p>)} {/* default */}
-            {jsonResult && !isUploading && (<p>Upload another Vehicle</p>)} {/* after finishing  */}
+            {!jsonResult && !isUploading && (<p>Upload file</p>)}
+            {jsonResult && !isUploading && (<p>Upload another Vehicle</p>)}
             {isUploading && <p className="flex justify-center"><LoaderCircle className="animate-spin" /></p>}
           </button>
         )}
